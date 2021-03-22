0284b56f7e8f6a57ba85c5281468365c4ecf3b3a
mails4 <81127849+mails4@users.noreply.github.com>
2021-03-23 00:55:36 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/do-nk/',
+      '/do-nk/index.html',
+      '/do-nk/index.js',
+      '/do-nk/style.css',
+     
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
