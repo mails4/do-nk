1aba93588bff545e164af04a422a784de4953b33
mails4 <81127849+mails4@users.noreply.github.com>
2021-03-23 01:17:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
+
+// Files to cache
+const cacheName = 'PWA-v1';
+const appShellFiles = [
       '/do-nk/',
       '/do-nk/index.html',
       '/do-nk/index.js',
-      
-     
-    ])),
-  );
+];
+const gamesImages = [];
+for (let i = 0; i < games.length; i++) {
+  gamesImages.push(`data/img/${games[i].slug}.jpg`);
+}
+const contentToCache = appShellFiles.concat(gamesImages);
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
 });
 
+// Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
