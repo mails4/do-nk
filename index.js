73c89f24a2544494f66d5b1dc09551f9f0e1c0ea
mails4 <81127849+mails4@users.noreply.github.com>
2021-05-20 03:29:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Register service worker to control making site work offline
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
+// if ('serviceWorker' in navigator) {
+//  navigator.serviceWorker
+//    .register('/sw.js')
+//    .then(() => { console.log('Service Worker Registered'); });
+//}
+
+
+    if ("serviceWorker" in navigator) {
+        self.addEventListener("load", async () => {
+            const container = navigator.serviceWorker;
+            if (container.controller === null) {
+                const reg = await container.register("sw.js");
+            }
+        });
+    }
 
 // Code to handle install prompt on desktop
 
