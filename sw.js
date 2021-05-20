cc329806f9f24b40ac26a566708178d89f98ba55
mails4 <81127849+mails4@users.noreply.github.com>
2021-05-20 03:02:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-
-     
-     self.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
