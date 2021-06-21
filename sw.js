3f5acd9f1633f60ae23f358ba4e77e28da555709
mails4 <81127849+mails4@users.noreply.github.com>
2021-06-21 17:53:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_STATIC = 'static-cache-v3.3';
+const CACHE_STATIC = 'static-cache-v3.31';
 
 // Очищает старый кэш (не забудь апнуть версию кэша при апдейте)
 self.addEventListener('activate', event => {
